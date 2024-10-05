86e3412173fb1ec615df9a317e26dd73feb9f9a6
davidRamirez1005 <ramirezjd1005@gmail.com>
2024-10-04 23:21:55 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,16 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     })
-  ]
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  }
 })
